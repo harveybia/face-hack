3b62b363331d4ef1d7f281a357ace76a6b8ff647
harveybia <harvey.shi@yahoo.com>
2016-02-06 16:39:44 -0500

    Browser

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 import pickle
+import webbrowser
 
 def saveUserDict(dct):
     pickle.dump(dct, open('user.db', 'wb'))
 
 def loadUserDict():
     return pickle.load(open('user.db', 'wb'))
+
+def newBrowserTab(url):
+    webbrowser.open(url, new=1)
+
+if __name__ == "__main__":
+    webbrowser.open('http://www.google.com', new=1)
